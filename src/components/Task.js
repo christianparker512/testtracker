998b608ab1ab7b85cb3ea559d10fbe09f6ead6e8
Christian Parker <christianparker512@gmail.com>
2021-07-07 10:21:32 -0500

    font awesome x icons for deletion

--- src/components/Task.js
+++ src/components/Task.js
@@ -1,17 +1,10 @@
-import { FaTimes } from 'react-icons/fa'
+import {FaTimes} from 'react-icons/fa'
 
-const Task = ({ task, onDelete, onToggle }) => {
+const Task = ({task}) => {
     return (
-        <div
-            className={`task ${task.reminder && 'reminder'}`}
-            onDoubleClick={() => onToggle(task.id)}
-        >
+        <div className={'task'}>
             <h3>
-                {task.text}{' '}
-                <FaTimes
-                    style={{ color: 'red', cursor: 'pointer' }}
-                    onClick={() => onDelete(task.id)}
-                />
+                {task.text} <FaTimes />
             </h3>
             <p>{task.day}</p>
         </div>
