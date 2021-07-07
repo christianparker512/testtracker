57f774283b5ce5e6020bd4e4ead6c255981e00b1
Christian Parker <christianparker512@gmail.com>
2021-07-06 19:54:05 -0500

    buttons of different colors as components

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,11 +1,16 @@
 //Dont really need this any more due to updates
 import React from 'react'
+import PropTypes from 'prop-types'
+import Button from './Button'
 
 const Header = (props) => {
     return (
-        <header>
+        <header className='header'>
             {/*<h1 style={headingStyle}>{props.title}</h1>*/}
             <h1>{props.title}</h1>
+            <Button color='green' text='Press The Green one'/>
+            <Button color='blue' text='Press The Blue one'/>
+            <Button color='red' text='Press The Red one'/>
         </header>
     )
 }
@@ -13,12 +18,12 @@ const Header = (props) => {
 Header.defaultProps = {
     title: 'Task Tracker',
 }
-// Header.propTypes = {
-//     title: PropTypes.string.isRequired,
-// }
-// const headingStyle = {
-//     color: 'darkorange',
-//     backgroundColor: 'black'
-// }
+Header.propTypes = {
+    title: PropTypes.string.isRequired,
+}
+const headingStyle = {
+    color: 'darkorange',
+    backgroundColor: 'black'
+}
 
 export default Header
